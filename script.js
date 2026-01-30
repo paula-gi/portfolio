b56524dc81cbe0dcdfa399b732441833985dcb1c
Paula García Iglesias <paulagariglesias@gmail.com>
2026-01-30 17:15:07 +0100

    chore: update comments in script.js and style.css

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+
 const toggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
 
 toggle.addEventListener('click', () => {
   menu.classList.toggle('active');
 });
+
 
